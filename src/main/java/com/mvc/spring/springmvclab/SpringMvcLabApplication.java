905d72b0500e2314579e4dddc9425bc49036be82
Dima Matroskin <dima.matroskin062@gmail.com>
2019-01-15 18:11:55 +0200

    initial commit

---
+++ src/main/java/com/mvc/spring/springmvclab/SpringMvcLabApplication.java
@@ -0,0 +1,14 @@
+package com.mvc.spring.springmvclab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcLabApplication.class, args);
+	}
+
+}
+
