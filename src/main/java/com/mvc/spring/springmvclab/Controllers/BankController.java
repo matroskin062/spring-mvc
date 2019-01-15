17d95fc74309aa14b8cf07d25f40ddd4f08e244f
Dima Matroskin <dima.matroskin062@gmail.com>
2019-01-16 01:17:01 +0200

    added delete method

--- src/main/java/com/mvc/spring/springmvclab/Controllers/BankController.java
+++ src/main/java/com/mvc/spring/springmvclab/Controllers/BankController.java
@@ -4,9 +4,12 @@ import com.mvc.spring.springmvclab.Repositories.BankRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.jws.WebParam;
 
 @Controller
 @RequestMapping("/bank")
@@ -20,6 +23,12 @@ public class BankController {
         model.setViewName("allBanks");
         model.addObject("banks", bankRepo.findAll());
         return model;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/delete/{id}")
+    public ModelAndView delete(@PathVariable Long id){
+        bankRepo.deleteById(id);
+        return new ModelAndView("redirect:/bank");
     }
 
 }
