f64186d3e33629ba45886ae0ee62f93410fe48bc
Dima Matroskin <dima.matroskin062@gmail.com>
2019-01-17 02:40:57 +0200

    added delete method

--- src/main/java/com/mvc/spring/springmvclab/Controllers/ClientController.java
+++ src/main/java/com/mvc/spring/springmvclab/Controllers/ClientController.java
@@ -4,8 +4,10 @@ import com.mvc.spring.springmvclab.Entities.Client;
 import com.mvc.spring.springmvclab.Repositories.BankRepo;
 import com.mvc.spring.springmvclab.Repositories.ClientRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -21,6 +23,9 @@ public class ClientController {
     @Autowired
     private BankRepo bankRepo;
 
+    public ClientController() {
+    }
+
     @RequestMapping
     public ModelAndView getAll(ModelAndView modelAndView){
         modelAndView.setViewName("allClients");
@@ -34,6 +39,12 @@ public class ClientController {
                 .nextLong(1, (bankRepo.count() + 1))).get());
 
         clientRepo.save(client);
+        return new ModelAndView("redirect:/client");
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/delete/{id}")
+    public ModelAndView delete(@PathVariable Long id){
+        clientRepo.deleteById(id);
         return new ModelAndView("redirect:/client");
     }
 }
