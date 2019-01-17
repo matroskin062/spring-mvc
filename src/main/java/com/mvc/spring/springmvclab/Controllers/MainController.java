b6545cf887abb349cd3b654eb8040889a07e6f4f
Dima Matroskin <dima.matroskin062@gmail.com>
2019-01-17 02:17:56 +0200

    MainController created

---
+++ src/main/java/com/mvc/spring/springmvclab/Controllers/MainController.java
@@ -0,0 +1,15 @@
+package com.mvc.spring.springmvclab.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/main")
+public class MainController {
+    @RequestMapping(method = RequestMethod.GET)
+    public String toMain(){
+        return "main";
+    }
+}
