d617dfc6f0c2cd74643dc3b800e54d374dc177ae
Dima Matroskin <dima.matroskin062@gmail.com>
2019-01-18 00:47:59 +0200

    mapping changed

--- src/main/java/com/mvc/spring/springmvclab/Controllers/MainController.java
+++ src/main/java/com/mvc/spring/springmvclab/Controllers/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping({"/","/main"})
+@RequestMapping("/")
 public class MainController {
     @RequestMapping(method = RequestMethod.GET)
     public String toMain(){
