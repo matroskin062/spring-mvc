9d7686bc5dec913fcfc87e1fe190aea55dc3711a
Dima Matroskin <dima.matroskin062@gmail.com>
2019-01-19 15:50:03 +0200

    added error handling

--- src/main/java/com/mvc/spring/springmvclab/Controllers/SecurityController.java
+++ src/main/java/com/mvc/spring/springmvclab/Controllers/SecurityController.java
@@ -1,6 +1,7 @@
 package com.mvc.spring.springmvclab.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -8,7 +9,9 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class SecurityController {
     @RequestMapping(method = RequestMethod.GET, value = "/login")
-    public ModelAndView loginPage(){
+    public ModelAndView loginPage(Model modelAndView, String error){
+        if(error != null)
+            modelAndView.addAttribute("error", "Username or password invalid");
         return new ModelAndView("login");
     }
 }
