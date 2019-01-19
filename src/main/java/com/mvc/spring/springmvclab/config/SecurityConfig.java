715ca43a5fa666accbfef1182d84a69dc2b710bb
Dima Matroskin <dima.matroskin062@gmail.com>
2019-01-19 14:34:26 +0200

    added inmemory auth

--- src/main/java/com/mvc/spring/springmvclab/config/SecurityConfig.java
+++ src/main/java/com/mvc/spring/springmvclab/config/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/login").permitAll()
+                .antMatchers("/login").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
